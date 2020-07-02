f2650da6cc1f2f4cb55b4cbe82bdf7c4d8eacccc
sanix sandel <sanicksikani@gmail.com>
2020-07-02 22:17:31 +0300

    viewsets and routers

--- blog/posts/urls.py
+++ blog/posts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import PostList, PostDetail, UserList, UserDetail
+from rest_framework.routers import SimpleRouter
 
-urlpatterns = [
-    path('users/', UserList.as_view()),
-    path('users/<int:pk>/', UserDetail.as_view()),
-    path('<int:pk>/', PostDetail.as_view()),
-    path('', PostList.as_view()),
-]
+from .views import UserViewSet, PostViewSet
+
+
+router=SimpleRouter()
+router.register('users', UserViewSet, basename='users')
+router.register('', PostViewSet, basename='posts')
+urlpatterns=router.urls
--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,42 +1,20 @@
-from rest_framework import generics, permissions
+from rest_framework import viewsets
 from django.contrib.auth import get_user_model
+
 from .models import Post
 from .permissions import IsAuthorOrReadOnly
 from .serializers import PostSerializer, UserSerializer
 
 
 
-
-class PostList(generics.ListCreateAPIView):
-    #permission_classes=(permissions.IsAuthenticated,)
-    queryset=Post.objects.all()
-    serializer_class=PostSerializer
-
-
-
-class PostDetail(generics.RetrieveUpdateDestroyAPIView):
-    #perimission_classes=(permissions.IsAuthenticated,)
+class PostViewSet(viewsets.ModelViewSet):
     permission_classes=(IsAuthorOrReadOnly,)
     queryset=Post.objects.all()
     serializer_class=PostSerializer
 
 
-class UserList(generics.ListCreateAPIView):
+class UserViewSet(viewsets.ModelViewSet):
+    #perimission_classes=(permissions.IsAuthenticated,)
+    #permission_classes=(IsAuthorOrReadOnly,)
     queryset=get_user_model().objects.all()
     serializer_class=UserSerializer
-
-
-class UserDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset=get_user_model().objects.all()
-    serializer_class=UserSerializer
-
-"""
-If you notice, there is quite a bit of repetition here.
- Both Post views and User views
-have the exact same queryset and serializer_class .
- Maybe those could be combined
-in some way to save code?
-Finally we have our URL routes. Make sure to import our
- new UserList , and UserDetail
-views. Then we can use the prefix users/ for each.
-"""
