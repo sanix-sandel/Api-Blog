fcb63878aae49b20a589ae038102e08028a09014
sanix sandel <sanicksikani@gmail.com>
2020-07-02 22:01:15 +0300

    user endpoints

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,8 +1,10 @@
 from rest_framework import generics, permissions
-
+from django.contrib.auth import get_user_model
 from .models import Post
 from .permissions import IsAuthorOrReadOnly
-from .serializers import PostSerializer
+from .serializers import PostSerializer, UserSerializer
+
+
 
 
 class PostList(generics.ListCreateAPIView):
@@ -17,3 +19,24 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes=(IsAuthorOrReadOnly,)
     queryset=Post.objects.all()
     serializer_class=PostSerializer
+
+
+class UserList(generics.ListCreateAPIView):
+    queryset=get_user_model().objects.all()
+    serializer_class=UserSerializer
+
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset=get_user_model().objects.all()
+    serializer_class=UserSerializer
+
+"""
+If you notice, there is quite a bit of repetition here.
+ Both Post views and User views
+have the exact same queryset and serializer_class .
+ Maybe those could be combined
+in some way to save code?
+Finally we have our URL routes. Make sure to import our
+ new UserList , and UserDetail
+views. Then we can use the prefix users/ for each.
+"""
